e301f36a6f2231a74563719e78b267a1eb219af4
Gutyiii <gutyeszbandesz@gmail.com>
2022-03-18 14:20:44 +0100

    static metodus() + hivas

--- src/pkg03/pkg18/Main.java
+++ src/pkg03/pkg18/Main.java
@@ -16,6 +16,13 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+       metodus();
+        
     }
-    
+
+    static void metodus() {
+        System.out.println("metodus");
+    }
+
 }
